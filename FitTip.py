5a59b1fd1acd348001658a0ca25faaaae5d2c9a8
Girish Sarvankar <140968675+GirishSarvankar@users.noreply.github.com>
2024-12-16 08:06:07 +0530

    Add files via upload

---
+++ FitTip.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title('FitTip: Squats Analysis')
+
+
+recorded_file = 'output_sample(1).mp4'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+    
+    
+
+    
+
+
